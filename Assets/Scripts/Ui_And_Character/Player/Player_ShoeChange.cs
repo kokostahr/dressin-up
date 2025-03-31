a5f53923ef42651f24c5a05af245d1a42ceb2961
kokostahr <starking.nn@gmail.com>
2025-03-31 22:56:59 +0200

    created folders and scripts for the changing of outfits

--- Assets/Scripts/Ui_And_Character/Player/Player_ShoeChange.cs
+++ Assets/Scripts/Ui_And_Character/Player/Player_ShoeChange.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerOutFitChanger : MonoBehaviour
+public class Player_ShoeChange : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
