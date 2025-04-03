ecb4966c52e24576f404198aa1cb24056152ea06
kokostahr <starking.nn@gmail.com>
2025-04-04 00:41:36 +0200

    made scriptable objects for all the items

---
+++ Assets/Scripts/PointSystem/ScriptableObjects/ClothingItemData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NewClothingItem", menuName ="Clothing System/ClothingItem")]
+public class ClothingItemData : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemIcon; //For the UI display
+    public int winterPoints; //For the relevant points associated with Winter theme
+
+}
