eb8b9ae6d7578f0d624e77541cbc61da0be0be5a
kokostahr <starking.nn@gmail.com>
2025-04-17 10:22:56 +0200

    added more theme related points and an item tag data to the scriptable objects.

--- Assets/Scripts/PointSystem/ScriptableObjects/ClothingItemData.cs
+++ Assets/Scripts/PointSystem/ScriptableObjects/ClothingItemData.cs
@@ -8,4 +8,10 @@ public class ClothingItemData : ScriptableObject
     public Sprite itemIcon; //For the UI display
     public int winterPoints; //For the relevant points associated with Winter theme
     public int summerPoints; //for the relevant points associated with Summer theme
+    public int casualPoints; //for the relevant points associated with a Casual theme
+    public int cozyPoints; //For the relevant points associated with a cozy theme
+    public string[] itemTag; //Made it an array so I can add more than one tag.
+
+
+
 }
